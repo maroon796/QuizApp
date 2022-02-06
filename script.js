6f1c8687fa8d686e35bde6817f889ae4c25f6d4c
Grygory Ksheminsky <maroon785@gmail.com>
2022-02-06 21:24:18 +0200

    finding a questions for the quiz

---
+++ script.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const quizInfo = [
+  {
+    question: 'Katerina Sakellaropoulou was elected the first woman President of',
+    a: 'Greece',
+    b: 'Spain',
+    c: 'Finland',
+    d: 'Netherland',
+    correct: 'a',
+  },
+
+  {
+    question: 'Which one among the following radiations carries maximum energy?',
+    a: 'Ultraviolet rays',
+    b: 'Gamma rays',
+    c: 'X- rays',
+    d: 'Infra-red rays',
+    correct: 'b',
+  },
+
+  {
+    question: 'The head quarters of world trade organization is in',
+    a: 'Montreal',
+    b: 'Seattle',
+    c: 'Geneva',
+    d: 'the Hague',
+    correct: 'c',
+  },
+
+  {
+    question: 'Which country hosted the football World Cup in 2014?',
+    a: 'China',
+    b: 'China',
+    c: 'Japan',
+    d: 'Brazil',
+    correct: 'd',
+  },
+
+  {
+    question: 'Nobel prize is awarded for which of the following disciplines:',
+    a: 'Literature, peace and economics',
+    b: 'Medicine or Physiology',
+    c: 'Chemistry and Physics',
+    d: 'All the above',
+    correct: 'd',
+  },
+];
