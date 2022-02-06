7c72f1b2cdf684222f0a12762dd3709b3e3de831
Grygory Ksheminsky <maroon785@gmail.com>
2022-02-06 21:30:37 +0200

    saving all the elements in variables

--- script.js
+++ script.js
@@ -46,3 +46,12 @@ const quizInfo = [
     correct: 'd',
   },
 ];
+
+const quiz = document.getElementById('quiz');
+const answerElements = document.querySelectorAll('.answer');
+const questionElement = document.getElementById('question');
+const a_text = document.getElementById('a_text');
+const b_text = document.getElementById('b_text');
+const c_text = document.getElementById('c_text');
+const d_text = document.getElementById('d_text');
+const submit = document.getElementById('submit');
